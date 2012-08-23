ca13f18e79fdbe48e9f97cb3a08dce09bd8bcf46
skrusche63 <skrusche63@googlemail.com>
2012-08-23 13:12:45 +0200

    Constants added.

---
+++ ames-web-shared/src/de/kp/ames/web/shared/constants/ApplicationConstants.java
@@ -0,0 +1,59 @@
+package de.kp.ames.web.shared.constants;
+/**
+ *	Copyright 2012 Dr. Krusche & Partner PartG
+ *
+ *	AMES-Web-Shared is free software: you can redistribute it and/or 
+ *	modify it under the terms of the GNU General Public License 
+ *	as published by the Free Software Foundation, either version 3 of 
+ *	the License, or (at your option) any later version.
+ *
+ *	AMES- Web-Shared is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * 
+ *  See the GNU General Public License for more details. 
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this software. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+public class ApplicationConstants {
+
+	public static String FNC_ID = "urn:oasis:names:tc:ebxml-regrep:FNC";	
+	public static String FNC_ID_App = FNC_ID   + ":App";	
+	
+	/*
+	 * Classification of bulletin board
+	 */
+	public static String FNC_APP_ID_Bulletin = FNC_ID_App + ":Bulletin";	
+	/*
+	 * Classification of chat application
+	 */
+	public static String FNC_APP_ID_Chat = FNC_ID_App + ":Chat";	
+	/*
+	 * Classification of desktop
+	 */
+	public static String FNC_APP_ID_Desktop = FNC_ID_App + ":Desktop";	
+	/*
+	 * Classification of online help
+	 */
+	public static String FNC_APP_ID_Help = FNC_ID_App + ":Help";
+	/*
+	 * Classification of mail application
+	 */
+	public static String FNC_APP_ID_Mail = FNC_ID_App + ":Mail";	
+	/*
+	 * Classification of portal
+	 */
+	public static String FNC_APP_ID_Portal = FNC_ID_App + ":Portal";
+	/*
+	 * Classification of Source Code Management System
+	 */
+	public static String FNC_APP_ID_ScmSys = FNC_ID_App + ":ScmSys";
+	/*
+	 * Classification of ShowCase
+	 */
+	public static String FNC_APP_ID_ShowCase = FNC_ID_App + ":ShowCase";
+
+}
